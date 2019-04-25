cba05365179995bd702388998c4e365a01516d66
Elizabeth Perez <47160130+elizabethseis@users.noreply.github.com>
2019-04-25 16:50:19 -0500

    Commit

---
+++ Exercise 2 - Development C/Execution.cs
@@ -0,0 +1,31 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Exercise_2___Development_C
+{
+    class Execution
+    {
+        public void execution()
+        {
+            Program obj1 = new Program();
+            obj1.WaitForElement(By.XPath("//div[@class='mbl _3m9 _6o _6s _mf']"), 5);
+            string actualTittle = Program.driver.Title;
+            string Text = "Facebook - Log In or Sign Up";
+            Console.WriteLine(actualTittle);
+            Program obj3 = new Program();
+            obj3.VAsserts(actualTittle, Text);
+            Program obj5 = new Program();
+            obj5.WaitForElement(By.XPath("//h2[@class='inlineBlock _3ma _6n _6s _6v']"), 5);
+            string ValidateText = Program.driver.FindElement(By.XPath("//h2[@class='inlineBlock _3ma _6n _6s _6v']")).Text;
+            string ExpectedText = "Connect with friends and the\r\nworld around you on Facebook.";
+            Program obj6 = new Program();
+            obj6.VAsserts(ValidateText, ExpectedText);
+        }
+
+    }
+}
--- Exercise 2 - Development C/Program.cs
+++ Exercise 2 - Development C/Program.cs
@@ -3,19 +3,84 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using NUnit.Framework;
 namespace Exercise_2___Development_C
 {
     class Program
     {
-        static void Main(string[] args)
+        public static IWebDriver driver;
+        public static void Main(string[] args)
         {
-            // The code provided will print ‘Hello World’ to the console.
-            // Press Ctrl+F5 (or go to Debug > Start Without Debugging) to run your app.
-            Console.WriteLine("Hello World!");
-            Console.ReadKey();
+            OpenBrowser();
+            Execution obj2 = new Execution();
+            obj2.execution();
+            FillSignUp();
+            Birthday();
+            Console.ReadLine();
+        }
 
-            // Go to http://aka.ms/dotnet-get-started-console to continue learning how to build a console app! 
+        public static void OpenBrowser()
+        {
+            driver = new ChromeDriver();
+            driver.Navigate().GoToUrl("https://www.facebook.com");
+        }
+        public void WaitForElement(By Locator, int seconds)
+        {
+            try
+            {
+                new WebDriverWait(driver, TimeSpan.FromSeconds(seconds)).Until(ExpectedConditions.ElementIsVisible(Locator));
+            }
+            catch (Exception i)
+            {
+                Console.WriteLine("Element is not displayed");
+            }
+        }
+
+        public static void Click(IWebElement element1)
+        {
+            element1.Click();
+        }
+
+        public void VAsserts(string element1, string element2)
+        {
+            try
+            {
+                Assert.AreEqual(element1, element2);
+                Console.WriteLine("Assert is correct");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+        } 
+
+        public static void FillSignUp()
+        {
+            IWebElement Name = driver.FindElement(By.XPath("//input[@name='firstname']"));
+            Name.SendKeys("ELIZABETH");
+            IWebElement LastName = driver.FindElement(By.XPath("//input[@name='lastname']"));
+            LastName.SendKeys("PEREZ");
+            IWebElement Phone = driver.FindElement(By.XPath("//input[@name='reg_email__']"));
+            Phone.SendKeys("4777902377");
+            IWebElement Password = driver.FindElement(By.XPath("//input[@name='reg_passwd__']"));
+            Password.SendKeys("Open12345");
+            
+        }
+        public static void Birthday()
+        {
+            SelectElement DrpDay = new SelectElement(driver.FindElement(By.XPath("//select[@id='day']")));
+            SelectElement DrpMonth = new SelectElement(driver.FindElement(By.XPath("//select[@id='month']")));
+            SelectElement DrpYear = new SelectElement(driver.FindElement(By.XPath("//select[@id='year']")));
+            DrpDay.SelectByValue("21");
+            DrpMonth.SelectByValue("8");
+            DrpYear.SelectByValue("1988");
+            Program obj4 = new Program();
+            obj4.WaitForElement(By.XPath("//label[contains(text(),'Female')]"), 5);
+            IWebElement RBFemale = driver.FindElement(By.XPath("//label[contains(text(),'Female')]"));
+            Click(RBFemale);
         }
     }
 }
